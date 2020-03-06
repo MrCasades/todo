fc6f0c3ada0e3d97b0ea76767373807023622ba8
MrCasades <casades@list.ru>
2020-03-07 00:26:33 +0300

    update form

--- src/components/item-add-form/item-add-form.js
+++ src/components/item-add-form/item-add-form.js
@@ -4,20 +4,35 @@ import './item-add-form.css';
 
 export default class ItemADddForm extends React.Component {
 
+    state = {
+        label: ''//инициализируем изменённое состояние
+    }
+
     /*Получение значения из формы*/
-    onLabelChange = (event) => {
-        console.log ('.')
+    onLabelChange = (e) => {
+        this.setState({
+            label: e.target.value
+        })
+    }
+
+    onSubmit = (e) => {
+        e.preventDefault()//отмена действия по умолчанию
+        this.props.onAddItem (this.state.label)
+        this.setState({
+            label: ''//инициализируем изменённое состояние
+        })
     }
 
     render(){
         return (
-            <form className = "item-add-form d-flex">
+            <form className = "item-add-form d-flex"
+             onSubmit = {this.onSubmit}>
                 <input type="text" 
                         className = "form-control"
-                        onChange = {this.onLabelChange}
-                        placeholder = "Your text..."/>
-                <button className = "btn btn-outline-secondary"
-                onClick = {() => this.props.onAddItem ('Hello!')}>
+                        onChange = {this.onLabelChange}//метод получает данные из формы
+                        placeholder = "Your text..."
+                        value = {this.state.label}/>
+                <button className = "btn btn-outline-secondary">
                     Add
                 </button>
             </form>
